964e38e50209f90694f7e9707c1a17ba7b49c367
nssuperx <mgtmgtx@gmail.com>
2019-11-14 09:06:36 +0900

    メモを消した

--- VRgame_2019univfes/Assets/Scripts/UnityChanScripts/UDPReceiver.cs
+++ VRgame_2019univfes/Assets/Scripts/UnityChanScripts/UDPReceiver.cs
@@ -39,13 +39,10 @@ public class UDPReceiver : MonoBehaviour
     }
 
     public string GetrawText(){
-        
         string receiveRawText;
         receiveRawText = rawText;
         rawText = "";
         return receiveRawText;
-        
-        //return rawText;
     }
 }
 
