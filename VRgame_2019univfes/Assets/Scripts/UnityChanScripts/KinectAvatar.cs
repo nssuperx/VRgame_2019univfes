ba33945ee527be045bafe7194665442d9e735b33
nssuperx <mgtmgtx@gmail.com>
2019-11-14 09:06:36 +0900

    メモを消した

--- VRgame_2019univfes/Assets/Scripts/UnityChanScripts/KinectAvatar.cs
+++ VRgame_2019univfes/Assets/Scripts/UnityChanScripts/KinectAvatar.cs
@@ -7,15 +7,6 @@ using System;
 このクラスはunityちゃんを動かします
 **********************************/
 
-/*********************************
-やることリスト
-0. 新しい値が来てるとき来てないときで処理をしっかり分ける。 <- いまここ
-0.1 新しい値来てないときとれるstring(rawStr)の値確認
-    前の値はもちろん残ったまんま
-1. unityちゃん本体の回転初期化処理を止めてみる。
-2. Quaternion線形補完を試す
-**********************************/
-
 public class KinectAvatar : MonoBehaviour {
 
     //ネットワーク関連
@@ -80,44 +71,12 @@ public class KinectAvatar : MonoBehaviour {
     void Update () {
 
         Quaternion q;
-        //Quaternion[] receiveQuaternion = new Quaternion[receiveQuaternionNum];
 
         string[] splitText = udpReceiver.GetrawText().Split('_');
-        //受信できてないときどうなってんのか確認
-        //ずっと値が入ってる
 
-        /****************
-        めも
-        返ってくる値がstaticなのでずっと前の値が残ってる
-        それで下のif文が走る
-        それで、UDPReceiver側で値が更新されてないとき(udp.Receiveが走ってないとき)は、
-        空の文字列が返ってくるようにして、値が来てないときは下のif文が走らんようにした。
-        そしたら動きががくがくになった。（当然だけど）
-        動きと回転の更新は常に続ける必要がある。
-
-        使えそうな方法:quaternionの線形補完
-        Time.deltatimeでいい感じに補完する
-        30fpsで値が来ることを信じる。
-        0sのとき0
-        1/30sのとき1
-        になるように補完する
-        dt = Time.deltaTime * 30;
-        pre = Quaternion.Lerp(pre,now,dt);
-        *****************/
-
-        // 関節の回転を取得する
+        //ここで飛んできた値を気合でパース
         if (splitText.Length >= receiveQuaternionNum + 1)
         {
-            dt = 0f;
-            // 回転の初期化
-            //ここ試しにコメント化してみよう
-            //関係なかった
-            q = transform.rotation;
-            transform.rotation = Quaternion.identity;
-
-            
-            //ここで飛んできた値を気合でパース
-            
             //ここはQuaternion
             for(int i=0;i<receiveQuaternionNum;i++){
                 string[] quaternionStr = splitText[i].Split(',');
@@ -128,13 +87,12 @@ public class KinectAvatar : MonoBehaviour {
             //ここはunityちゃんのposition
             string[] rawPosStr = splitText[11].Split(',');
             rawPos = new Vector3(float.Parse(rawPosStr[0]),float.Parse(rawPosStr[1]),float.Parse(rawPosStr[2]));
-            
-            transform.rotation = q;
 
-
-        }else{
-            dt += Time.deltaTime * 30;
         }
+
+        // 回転の初期化
+        q = transform.rotation;
+        transform.rotation = Quaternion.identity; 
 
         Spine1.transform.rotation = Quaternion.Lerp(Spine1.transform.rotation,receiveQuaternion[0],0.1f);
         RightArm.transform.rotation = Quaternion.Lerp(RightArm.transform.rotation,receiveQuaternion[1],lerpValue);
@@ -147,6 +105,8 @@ public class KinectAvatar : MonoBehaviour {
         RightLeg.transform.rotation = Quaternion.Lerp(RightLeg.transform.rotation,receiveQuaternion[8],lerpValue);
         LeftUpLeg.transform.rotation = Quaternion.Lerp(LeftUpLeg.transform.rotation,receiveQuaternion[9],lerpValue);
         LeftLeg.transform.rotation = Quaternion.Lerp(LeftLeg.transform.rotation,receiveQuaternion[10],lerpValue);
+
+        transform.rotation = q;
 
 
         //ローパスフィルター処理
