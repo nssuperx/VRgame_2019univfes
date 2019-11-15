021fc01993d616b8162d6b41643baa761b25c7f8
nef0608 <sigeren00@gmail.com>
2019-11-15 19:09:21 +0900

    安全域の調整中

---
+++ VRgame_2019univfes/Assets/Programs/GameScore.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScore : MonoBehaviour {
+
+    // スコアを表示する
+    public Text scoreText;
+    // ハイスコアを表示する
+    public Text highScoreText;
+
+    // スコア
+    private int score;
+
+    public int Score{
+        get{return score;}
+        private set{score = value;}
+    }
+
+    // ハイスコア
+    private int highScore;
+
+    public int HighScore{
+        get{return highScore;}
+        private set{highScore = value;}
+    }
+
+    // PlayerPrefsで保存するためのキー
+    private string highScoreKey = "highScore";
+
+    void Start ()
+    {
+        Initialize ();
+        DontDestroyOnLoad(this);
+    }
+
+    void Update ()
+    {
+        // スコアがハイスコアより大きければ
+        if (highScore < score) 
+        {
+            highScore = score;
+        }
+
+        // スコア・ハイスコアを表示する
+        scoreText.text = "Score:" + score.ToString ();
+        highScoreText.text = "HighScore:" + highScore.ToString ();
+    }
+
+    // ゲーム開始前の状態に戻す
+    private void Initialize ()
+    {
+        // スコアを0に戻す
+        score = 0;
+
+        // ハイスコアを取得する。保存されてなければ0を取得する。
+        highScore = PlayerPrefs.GetInt (highScoreKey, 0);
+    }
+
+    // ポイントの追加
+    public void AddPoint (int point)
+    {
+        score = score + point;
+    }
+
+    // ハイスコアの保存
+    public void Save ()
+    {
+        // ハイスコアを保存する
+        PlayerPrefs.SetInt (highScoreKey, highScore);
+        PlayerPrefs.Save ();
+
+        // ゲーム開始前の状態に戻す
+        Initialize ();
+    }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+    //衝突判定
+    if (collision.gameObject.tag == "core") 
+        {
+            
+            //スコア処理を追加
+            FindObjectOfType<GameScore>().AddPoint(20);
+
+        }
+    }
+}
---
+++ VRgame_2019univfes/Assets/Programs/Result/ReStart.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+ 
+public class ReStart : MonoBehaviour {
+ 
+	//　トータル制限時間
+	private float totalTime;
+	//　制限時間（分）
+	[SerializeField]
+	private int minute;
+	//　制限時間（秒）
+	[SerializeField]
+	private float seconds;
+	//　前回Update時の秒数
+	private float oldSeconds;
+ 
+	void Start () {
+		totalTime = minute * 60 + seconds;
+		oldSeconds = 0f;
+	}
+ 
+	void Update () {
+		//　制限時間が0秒以下なら何もしない
+		if (totalTime <= 1f) {
+			return;
+		}
+		//　一旦トータルの制限時間を計測；
+		totalTime = minute * 60 + seconds;
+		totalTime -= Time.deltaTime;
+ 
+		//　再設定
+		minute = (int) totalTime / 60;
+		seconds = totalTime - minute * 60;
+ 
+		oldSeconds = seconds;
+		//　制限時間以下になったら結果発表を行う
+		if(totalTime <= 1f) {
+			SceneManager.LoadScene("level selection");
+			Destroy(GameObject.Find("Plane"));
+		}
+	}
+}
+ 
